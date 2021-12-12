ab5a3ff7c22f8c59c476c3c9aa0dff605b8c8340
xiaoqiang jiang <xiaoqiangjiang@xiaoqiangs-MacBook-Pro.local>
2021-12-12 11:16:35 +0800

    [feature] add dashboard response

--- src/service/home/home_service.rs
+++ src/service/home/home_service.rs
@@ -2,7 +2,7 @@ use rust_wheel::config::db::config;
 use crate::models::{Dashboard};
 use crate::diesel::prelude::*;
 
-pub fn word_search_impl() -> Vec<Dashboard>{
+pub fn dashboard_impl() -> Vec<Dashboard>{
     use crate::schema::dashboard::dsl::*;
     let connection = config::establish_connection();
     let results = dashboard
