1d638cb5c429ad4901d9fef3e3d60390e4b2612a
xiaoqiang jiang <xiaoqiangjiang@xiaoqiangs-MacBook-Pro.local>
2021-12-19 01:00:45 +0800

    [feature] tweak fav page request

--- src/model/request/app/music/fav/fav_music_request.rs
+++ src/model/request/app/music/fav/fav_music_request.rs
@@ -4,7 +4,7 @@ use rocket::serde::Serialize;
 #[derive(Debug, PartialEq, Eq, Deserialize, Serialize)]
 #[allow(non_snake_case)]
 pub struct FavMusicRequest {
-    pub userId: i64,
+    pub userId: Option<i64>,
     pub pageNum: i64,
     pub pageSize: i64
 }
