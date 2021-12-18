6a7f2e7f8095f494a442e4470c7b0c0efe39f1e4
jiangxiaoqiang <jiangtingqiang@gmail.com>
2021-12-18 06:58:37 -0500

    [feature] tweak

--- src/biz/app/music/fav/fav_music_controller.rs
+++ src/biz/app/music/fav/fav_music_controller.rs
@@ -8,7 +8,7 @@ use crate::model::response::home::dashboard_response::DashboardResponse;
 use crate::models::{Dashboard, Favorites};
 use crate::service::home::home_service::fav_music_query;
 
-#[post("/v1/fav/page",data = "<request>")]
+#[post("/v1/page",data = "<request>")]
 pub fn page(request: Json<FavMusicRequest>) -> content::Json<String> {
     let fav_musics = fav_music_query::<Vec<Favorites>>(request);
     let res = ApiResponse {
