80b400b45ba2fb07c9cb52c2e28c6084f5fc0385
jiang qiang <jiangtingqiang@gmail.com>
2021-12-22 13:16:22 +0800

    [feature] add detail query

--- src/biz/app/cruise/article/article_controller.rs
+++ src/biz/app/cruise/article/article_controller.rs
@@ -3,9 +3,9 @@ use rocket::serde::json::Json;
 use rust_wheel::model::response::api_response::ApiResponse;
 use crate::model::diesel::dolphin::dolphin_models::Article;
 use crate::model::request::app::cruise::article::article_request::ArticleRequest;
-use crate::service::app::cruise::article::article_service::article_query;
+use crate::service::app::cruise::article::article_service::{article_detail_query, article_query};
 
-#[post("/v1/page",data = "<request>")]
+#[post("/v1/page", data = "<request>")]
 pub fn page(request: Json<ArticleRequest>) -> content::Json<String> {
     let musics = article_query::<Vec<Article>>(&request);
     let res = ApiResponse {
@@ -14,6 +14,26 @@ pub fn page(request: Json<ArticleRequest>) -> content::Json<String> {
     };
     let response_json = serde_json::to_string(&res).unwrap();
     return content::Json(response_json);
+}
+
+#[get("/v1/detail/<id>")]
+pub fn detail(id: i64) -> content::Json<String> {
+    let articles = article_detail_query(id);
+    let article = take(articles,0);
+    let res = ApiResponse {
+        result: article.unwrap(),
+        ..Default::default()
+    };
+    let response_json = serde_json::to_string(&res).unwrap();
+    return content::Json(response_json);
+}
+
+fn take<T>(mut vec: Vec<T>, index: usize) -> Option<T> {
+    if vec.get(index).is_none() {
+        None
+    } else {
+        Some(vec.swap_remove(index))
+    }
 }
 
 
--- src/service/app/cruise/article/article_service.rs
+++ src/service/app/cruise/article/article_service.rs
@@ -1,4 +1,4 @@
-use diesel::{ExpressionMethods, QueryDsl, QueryResult};
+use diesel::{ExpressionMethods, QueryDsl, QueryResult, RunQueryDsl};
 use rocket::serde::json::Json;
 use rust_wheel::common::query::pagination::PaginateForQueryFragment;
 use rust_wheel::common::util::model_convert::map_pagination_res;
@@ -10,10 +10,21 @@ use crate::model::diesel::dolphin::dolphin_models::Article;
 pub fn article_query<T>(request: &Json<ArticleRequest>) -> PaginationResponse<Vec<Article>> {
     use crate::model::diesel::dolphin::dolphin_schema::article::dsl::*;
     let connection = config::establish_connection();
-    let query = article.filter(editor_pick.eq(1))
+    let query = article
+        .filter(editor_pick.eq(1))
         .paginate(request.pageNum)
         .per_page(request.pageSize);
     let query_result: QueryResult<(Vec<_>, i64, i64)> = query.load_and_count_pages_total::<Article>(&connection);
     let page_result = map_pagination_res(query_result, request.pageNum, request.pageSize);
     return page_result;
+}
+
+pub fn article_detail_query(article_id: i64) -> Vec<Article> {
+    use crate::model::diesel::dolphin::dolphin_schema::article::dsl::*;
+    let connection = config::establish_connection();
+    let query = article.filter(id.eq(article_id))
+        .limit(1)
+        .load::<Article>(&connection)
+        .expect("query article id failed");
+    return query;
 }
