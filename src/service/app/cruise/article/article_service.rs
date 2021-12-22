d3d4a085f4c1058085073fa636d2c6bfea17adc4
jiang qiang <jiangtingqiang@gmail.com>
2021-12-22 12:05:00 +0800

    [feature] add chrono to fix compatible problem

--- src/service/app/cruise/article/article_service.rs
+++ src/service/app/cruise/article/article_service.rs
@@ -1,4 +1,4 @@
-use diesel::{delete, ExpressionMethods, QueryDsl, QueryResult};
+use diesel::{ExpressionMethods, QueryDsl, QueryResult};
 use rocket::serde::json::Json;
 use rust_wheel::common::query::pagination::PaginateForQueryFragment;
 use rust_wheel::common::util::model_convert::map_pagination_res;
@@ -6,7 +6,6 @@ use rust_wheel::model::response::pagination_response::PaginationResponse;
 use crate::model::request::app::cruise::article::article_request::ArticleRequest;
 use rust_wheel::config::db::config;
 use crate::model::diesel::dolphin::dolphin_models::Article;
-use crate::model::diesel::dolphin::dolphin_schema::article::dsl::article;
 
 pub fn article_query<T>(request: &Json<ArticleRequest>) -> PaginationResponse<Vec<Article>> {
     use crate::model::diesel::dolphin::dolphin_schema::article::dsl::*;
