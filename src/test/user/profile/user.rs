9694590d5fd4062e88bb39ca4bbb6b5c0c52d39b
jiangxiaoqiang <jiangtingqiang@gmail.com>
2021-12-26 04:15:32 -0500

    [feature] add mod passowrd test

--- src/test/user/profile/user.rs
+++ src/test/user/profile/user.rs
@@ -4,16 +4,26 @@ mod test {
 
     use std::env;
     use diesel::{Connection, ExpressionMethods, PgConnection, QueryDsl, RunQueryDsl};
+    use rocket::serde::json::Json;
     use rust_wheel::common::query::pagination::PaginateForQuerySource;
     use rust_wheel::config::db::config::establish_music_connection;
     use rust_wheel::model::response::api_response::ApiResponse;
     use crate::model::diesel::rhythm::rhythm_schema::favorites::dsl::favorites;
     use crate::model::diesel::rhythm::rhythm_schema::favorites::like_status;
+    use crate::model::request::user::password_request::PasswordRequest;
     use crate::models::Favorites;
+    use crate::service::user::user_service::password_edit;
 
     #[test]
-    fn page_test(){
-
+    fn user_test(){
+        let pr = PasswordRequest{
+            loginType: 0,
+            newPassword: "123456".to_string(),
+            oldPassword: "$mycruise123".to_string(),
+            userName: "+8615683761628".to_string()
+        };
+        let jpr = Json(pr);
+        let password_request = password_edit(&jpr);
     }
 }
 
