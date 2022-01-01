fe48f4aa12dcd4ecf9463c2dabd8f276c313ea53
jiangxiaoqiang <jiangtingqiang@gmail.com>
2022-01-01 03:45:00 -0500

    [feature] tweak

--- src/biz/app/cernitor/domain/domain_controller.rs
+++ src/biz/app/cernitor/domain/domain_controller.rs
@@ -1,6 +1,7 @@
 use rocket::response::content;
 use rocket::serde::json::Json;
-use rust_wheel::common::util::model_convert::box_rest_response;
+use regex::Regex;
+use rust_wheel::common::util::model_convert::{box_error_rest_response, box_rest_response};
 use rust_wheel::model::user::login_user_info::LoginUserInfo;
 use crate::model::diesel::dolphin::dolphin_models::{Domain};
 use crate::model::request::app::cernitor::domain::add_domain_request::AddDomainRequest;
@@ -15,6 +16,10 @@ pub fn page(request: Json<DomainRequest>) -> content::Json<String> {
 
 #[post("/v1/add", data = "<request>")]
 pub fn add(request: Json<AddDomainRequest>, login_user_info: LoginUserInfo) -> content::Json<String> {
+    let domain_regex = Regex::new(r"").unwrap();
+    if !domain_regex.is_match(&request.domainUrl) {
+        box_error_rest_response("", "00100100064008".parse().unwrap(), "domain url format error".parse().unwrap());
+    }
     add_domain(&request, login_user_info);
     return box_rest_response("ok");
 }
