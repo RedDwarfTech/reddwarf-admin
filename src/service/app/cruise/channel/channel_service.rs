5914db0c88fd064f581e779bed58b2192475a0c9
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-01-18 23:25:19 +0800

    [refactor] remove the pakcage path to make code more shorter

--- src/service/app/cruise/channel/channel_service.rs
+++ src/service/app/cruise/channel/channel_service.rs
@@ -6,7 +6,6 @@ use rust_wheel::common::util::model_convert::map_pagination_res;
 use rust_wheel::model::response::pagination_response::PaginationResponse;
 use rust_wheel::config::db::config;
 use crate::model::diesel::dolphin::dolphin_models::{RssSubSource};
-use crate::model::diesel::dolphin::dolphin_schema::article::channel_reputation;
 use crate::model::request::app::cruise::channel::channel_request::ChannelRequest;
 
 type DB = diesel::pg::Pg;
@@ -31,9 +30,10 @@ pub fn channel_query<T>(request: &Json<ChannelRequest>) -> PaginationResponse<Ve
 fn find_channel(request: &ChannelRequest) -> Box<dyn BoxableExpression<crate::model::diesel::dolphin::dolphin_schema::rss_sub_source::table, DB, SqlType=Bool> + '_> {
     use crate::model::diesel::dolphin::dolphin_schema::rss_sub_source::dsl::*;
     match request {
-        ChannelRequest { editorPick, .. } if editorPick.unwrap_or(0) == 1 => Box::new(crate::model::diesel::dolphin::dolphin_schema::rss_sub_source::dsl::editor_pick.eq(editorPick)),
-        ChannelRequest{ minimalReputation, ..} if minimalReputation.unwrap_or(0) > 0 => Box::new(crate::model::diesel::dolphin::dolphin_schema::rss_sub_source::dsl::reputation.gt(minimalReputation)),
-        _ => Box::new(crate::model::diesel::dolphin::dolphin_schema::rss_sub_source::dsl::editor_pick.eq(0))
+        ChannelRequest { editorPick, .. } if editorPick.unwrap_or(0) == 1 => Box::new(editor_pick.eq(editorPick)),
+        ChannelRequest { minimalReputation, ..} if minimalReputation.unwrap_or(0) > 0 => Box::new(reputation.gt(minimalReputation)),
+        ChannelRequest { excludeEditorPickChannel, ..} if excludeEditorPickChannel.unwrap_or(0) == 1 => Box::new(editor_pick.eq(excludeEditorPickChannel)),
+        _ => Box::new(editor_pick.eq(0))
     }
 }
 
