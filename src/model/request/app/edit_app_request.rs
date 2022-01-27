cbfeff0ba9a6196bd03ce431579210d554b4073c
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-01-27 21:42:18 +0800

    [feature] tweak

--- src/model/request/app/edit_app_request.rs
+++ src/model/request/app/edit_app_request.rs
@@ -4,7 +4,6 @@ use rocket::serde::Serialize;
 #[derive(Debug, PartialEq, Eq, Deserialize, Serialize)]
 #[allow(non_snake_case)]
 pub struct EditAppRequest {
-    pub remark: String,
     pub onlineStatus: i32,
     pub appId: i32
 }
