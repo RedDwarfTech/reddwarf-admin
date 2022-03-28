4c056690330e9e23dcdb97e27a4a1cda798e171d
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-03-28 11:19:59 +0800

    [feature] add dynamic project root path in the shell script

--- scripts/diesel/diesel-dolphin-model.sh
+++ scripts/diesel/diesel-dolphin-model.sh
@@ -4,11 +4,16 @@ set -u
 set -e
 set -x
 
+# https://stackoverflow.com/questions/71637346/option-to-get-the-rust-project-dir-in-shell-script
+# with the project dir
+# we can execute the script in any of the project folder
+PROJECT_DIR="$(dirname "$(cargo locate-project|jq -r .root)")"
+
 # https://stackoverflow.com/questions/70410103/how-to-make-diesel-auto-generate-model
 diesel_ext --derive Insertable,Queryable,Debug,Serialize,Deserialize,Default \
 --add-table-name \
 --import-types "rocket::serde::Serialize" \
 --import-types "serde::Deserialize" \
 --import-types "crate::model::diesel::dolphin::dolphin_schema::*" \
---schema-file ../../src/model/diesel/dolphin/dolphin_schema.rs --model > ../../src/model/diesel/dolphin/dolphin_models.rs
+--schema-file "${PROJECT_DIR}"/src/model/diesel/dolphin/dolphin_schema.rs --model > "${PROJECT_DIR}"/src/model/diesel/dolphin/dolphin_models.rs
 
