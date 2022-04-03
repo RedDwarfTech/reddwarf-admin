0a2ada420c14fbcde52b7eb946bbaa6e54aaa177
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-04-04 01:11:54 +0800

    [feature] add recommand channel api

---
+++ src/model/request/app/cruise/channel/pick_channel_request.rs
@@ -0,0 +1,13 @@
+use rocket::serde::Deserialize;
+use rocket::serde::Serialize;
+
+#[derive(Debug, PartialEq, Eq, Deserialize, Serialize)]
+#[allow(non_snake_case)]
+pub struct ChannelRequest {
+    pub userId: Option<i64>,
+    pub pageNum: Option<i64>,
+    pub pageSize: Option<i64>,
+    pub editorPick: Option<i32>,
+    pub minimalReputation: Option<i64>,
+    pub excludeEditorPickChannel: Option<i32>
+}
