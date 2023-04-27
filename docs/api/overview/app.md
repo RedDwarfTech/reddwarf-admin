593fc8c0465f73a74ddd453e1ca0e47a17233a9c
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-04-27 18:33:12 +0800

    fix: use the product id to filter record

---
+++ docs/api/overview/app.md
@@ -0,0 +1,8 @@
+
+#### 查询分页数据
+
+```bash
+curl -X POST http://localhost:8000/manage/app/overview/app/v1/page -H "Content-Type: application/json" -d '{"pageNum": 1,"pageSize":10}'
+```
+
+
--- src/service/app/app_service.rs
+++ src/service/app/app_service.rs
@@ -1,9 +1,9 @@
 use diesel::dsl::any;
-use rand::{Rng, thread_rng};
 use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
 use rocket::serde::json::Json;
 use rust_wheel::common::query::pagination::PaginateForQueryFragment;
-use rust_wheel::common::util::model_convert::{map_pagination_from_list};
+use rust_wheel::common::util::model_convert::map_pagination_from_list;
 use rust_wheel::common::util::time_util::get_current_millisecond;
 use rust_wheel::config::db::config;
 use rust_wheel::model::response::pagination_response::PaginationResponse;
@@ -19,29 +19,33 @@ use crate::model::response::app::app_response::AppResponse;
 pub fn app_query<T>(request: &Json<AppRequest>) -> PaginationResponse<Vec<AppResponse>> {
     use crate::model::diesel::dolphin::dolphin_schema::apps::dsl::*;
     let connection = config::establish_connection();
-    let query = apps.filter(id.gt(0))
+    let query = apps
+        .filter(id.gt(0))
         .order(created_time.desc())
-        .paginate(request.pageNum,false)
+        .paginate(request.pageNum, false)
         .per_page(request.pageSize);
-    let query_result: QueryResult<(Vec<_>, i64, i64)> = query.load_and_count_pages_total::<App>(&connection);
+    let query_result: QueryResult<(Vec<_>, i64, i64)> =
+        query.load_and_count_pages_total::<App>(&connection);
     let app_response = append_product_name(&query_result.as_ref().unwrap().0, &connection);
     let total = query_result.as_ref().unwrap().2;
-    let page_result = map_pagination_from_list(app_response, request.pageNum, request.pageSize,total);
+    let page_result =
+        map_pagination_from_list(app_response, request.pageNum, request.pageSize, total);
     return page_result;
 }
 
-pub fn append_product_name(apps: &Vec<App>, connection:&PgConnection) -> Vec<AppResponse>{
+pub fn append_product_name(apps: &Vec<App>, connection: &PgConnection) -> Vec<AppResponse> {
     use crate::model::diesel::dolphin::dolphin_schema::products::dsl::*;
-    let product_ids:Vec<i32> = apps.iter()
-        .map(|item| item.product_id)
-        .collect();
-    let products_result = products.filter(product_id.eq(any(product_ids)))
+    let product_ids: Vec<i32> = apps.iter().map(|item| item.product_id).collect();
+    let products_result = products
+        .filter(product_id.eq(any(product_ids)))
         .load::<Product>(connection)
         .expect("query product source failed");
     let mut app_res = Vec::new();
     for app_temp in apps {
-        let fetched_product_name:String = products_result.iter().filter(|prod| prod.id== app_temp.product_id)
-            .map(|channel|channel.product_name.to_string())
+        let fetched_product_name: String = products_result
+            .iter()
+            .filter(|prod| prod.product_id == app_temp.product_id)
+            .map(|channel| channel.product_name.to_string())
             .collect::<String>();
         let mut app_response = AppResponse::from(app_temp);
         app_response.product_name = fetched_product_name;
@@ -59,7 +63,7 @@ pub fn app_create(request: &Json<AddAppRequest>) {
         .map(char::from)
         .take(10)
         .collect();
-    let app = AppAdd{
+    let app = AppAdd {
         app_name: request.appName.to_string(),
         remark: request.remark.to_string(),
         created_time: current_time,
@@ -70,7 +74,7 @@ pub fn app_create(request: &Json<AddAppRequest>) {
         app_tag: None,
         app_id: rand_string,
         app_abbr: request.appAbbr.to_string(),
-        product_id: request.productId
+        product_id: request.productId,
     };
     diesel::insert_into(crate::model::diesel::dolphin::dolphin_schema::apps::table)
         .values(&app)
@@ -92,7 +96,6 @@ pub fn app_edit(request: &Json<EditAppRequest>) {
 pub fn app_detail(query_app_id: i32) -> App {
     use crate::model::diesel::dolphin::dolphin_schema::apps::dsl::*;
     let connection = config::establish_connection();
-    let app_result = apps.filter(id.eq(query_app_id))
-        .first::<App>(&connection);
+    let app_result = apps.filter(id.eq(query_app_id)).first::<App>(&connection);
     return app_result.unwrap();
 }
